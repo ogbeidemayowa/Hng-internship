9fd3c23e5490d859ea0a844bfe0c7904a1712646
Mayowa.o <michealogbeide19@gmail.com>
2019-09-08 15:48:14 +0100

    update

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$name = $_POST['fname'];
+$email = $_POST['email'];
+$title = $_POST['tofmsg'];
+$message = $_POST['msg'];
+
+
+if (isset($_POST['submit'])){
+    $x = fopen ("messages.txt", "a");
+    // concat. the form elements in order for easy readability
+    $contactmsg = "\n\nName: " . $name . "\nEmail: " . $email . "\nTitle: " . $title . "\nMessage: " . $message;
+    // wriring to submit your file
+    fwrite($x, $contactmsg);
+    fclose($x);
+} 
+?>
